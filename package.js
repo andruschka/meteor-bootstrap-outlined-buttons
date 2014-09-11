d2a9072ce84ed592bbd51957062e7195ba8e00ae
andrej fritz <a.fritz@betzold.de>
2014-09-11 10:26:37 +0200

    init

---
+++ package.js
@@ -0,0 +1,10 @@
+Package.describe({
+  summary: "Provides nice outlined buttons via classes btn-outlined-red/blue/green/default/orange for bootstrap 3.",
+  version: "1.0.0",
+  git: "https://github.com/andruschka/meteor-bootstrap-outlined-buttons.git"
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('METEOR@0.9.1.1');
+  api.addFiles('outlined-buttons.css', 'client');
+});
